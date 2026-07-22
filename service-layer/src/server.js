0ce84cf2ab89e59cfe05ace48d95f91ba04a027a
ARVIND SINGH <arvindsing@gmail.com>
2026-07-22 00:41:41 -0700

    Set default backend API URL if not provided

--- service-layer/src/server.js
+++ service-layer/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 const allowedOrigin = process.env.ALLOWED_ORIGIN || 'https://app-sj311react-poc-test.azurewebsites.net/';
-const backendApiUrl = process.env.BACKEND_API_URL;
+const backendApiUrl = process.env.BACKEND_API_URL || 'https://app-sj311react-api-poc-test.azurewebsites.net/';
 const requestTimeoutMs = Number(process.env.REQUEST_TIMEOUT_MS || 5000);
 
 app.use(helmet());
